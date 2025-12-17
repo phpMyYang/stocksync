214d68aa11d30d40d582a0ac461fe3e6412afa66
phpMyYang <jeremiahoccupation@gmail.com>
2025-12-17 15:31:14 +0800

    Updated Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/js/dashboard.js',],
+            input: [
+                "resources/css/app.scss",
+                "resources/js/app.js",
+                "resources/js/dashboard.js",
+            ],
             refresh: true,
         }),
     ],
+    // IDAGDAG MO ANG PART NA ITO PARA MAWALA ANG WARNINGS:
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler", // or "modern"
+                silenceDeprecations: [
+                    "color-functions",
+                    "global-builtin",
+                    "import",
+                    "legacy-js-api",
+                ],
+            },
+        },
+    },
 });
